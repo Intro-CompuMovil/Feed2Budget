3e6d46ea96d4ad1975a2ffc17f36108c8ba3fb77
Miguel Angel Gonzalez Rodriguez <miguelgonrod2004@gmail.com>
2024-03-26 23:06:26 -0500

    Added main page, login and register

---
+++ Feed2Budget/app/src/main/java/com/compumovil/feed2budget/Login.kt
@@ -0,0 +1,46 @@
+package com.compumovil.feed2budget
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val login = findViewById<Button>(R.id.loginButton)
+
+        login.setOnClickListener{
+            checkValues()
+        }
+    }
+
+    private fun checkValues(){
+        val username = findViewById<TextView>(R.id.userText)
+        val password = findViewById<TextView>(R.id.passwordText)
+        val errorMessage = findViewById<TextView>(R.id.errorMessage)
+
+        if(username.text.toString().isEmpty() || password.text.toString().isEmpty()){
+            errorMessage.text = "No puedes tener campos vacios"
+        }
+        else if(username.text.toString() == "miguel" && password.text.toString() == "1234"){
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+        else{
+            errorMessage.text = password.text
+        }
+    }
+}
---
+++ Feed2Budget/app/src/main/java/com/compumovil/feed2budget/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.compumovil.feed2budget
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val login = findViewById<Button>(R.id.loginButton)
+        val register = findViewById<Button>(R.id.registerButton)
+
+        login.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        register.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
